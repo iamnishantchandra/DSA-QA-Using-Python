3d8618ba50000bbf34381d54c7ef8f9701e17c20
Nishant Chandra <nishantchandra85@gmail.com>
2021-07-20 18:36:01 +0530

    squareSum

---
+++ 10X/Python/Intro/squareSum.py
@@ -0,0 +1,23 @@
+"""
+
+n=int(input())
+sum=int((n*(n+1)*((2*n)+1))/6)
+# s=int(sum)
+print(sum)
+
+"""
+"""
+    while(p<2):
+        multi=multi*i
+        p=p+1
+"""
+no=int(input())
+n=no+1
+p=0
+multi=1
+sum=0
+for i in range(n):  
+    multi=i*i
+    sum=sum+multi
+    multi=1
+print(sum)
