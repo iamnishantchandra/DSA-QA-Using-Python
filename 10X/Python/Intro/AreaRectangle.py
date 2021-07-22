82a569ac41bed106f4599a0dda9cdff79880ff96
Nishant Chandra <nishantchandra85@gmail.com>
2021-07-22 11:01:53 +0530

    area,perimeter

---
+++ 10X/Python/Intro/AreaRectangle.py
@@ -0,0 +1,7 @@
+a=int(input())
+b=int(input())
+area=int(a*b)
+if area>0:
+    print(area)
+else:
+    print(0)
---
+++ 10X/Python/Intro/perimeter.py
@@ -0,0 +1,4 @@
+length=int(input())
+breadth=int(input())
+perimeter=int(2*(length+breadth))
+print(perimeter)
