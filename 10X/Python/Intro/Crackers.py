e766d094bfb0a32662bab6c39eeb7e274ec5feb0
Nishant Chandra <nishantchandra85@gmail.com>
2021-07-24 11:45:00 +0530

    sum_1d_array and Crackers

---
+++ 10X/Python/Intro/Crackers.py
@@ -0,0 +1,7 @@
+a,b=map(int,input().split())
+r=a%b
+if r==0:
+    print(0)
+else:
+    print(1)
+
--- 10X/Python/String/sum_1d_array.py
+++ 10X/Python/String/sum_1d_array.py
@@ -1,1 +1,9 @@
-  
+s=input()
+l=len(s)
+n=list(s)
+p=[]
+sum=0
+for i in range(0,l):
+    sum=sum+int(n[i])
+    p.append(sum)
+print(p)
