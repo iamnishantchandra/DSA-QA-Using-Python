a5be5ff36dc4cd23f15780e0ee129638274ec159
Nishant Chandra <nishantchandra85@gmail.com>
2021-07-26 16:56:17 +0530

    Range

---
+++ 10X/Python/String/Range.py
@@ -0,0 +1,12 @@
+	m=int(input())
+n=int(input())
+if (m<=n and n>=0):
+
+    while(m<=n):
+        if(m>=0):
+            print(m)
+            m=m+1
+        else:
+            m=m+1
+else:
+    print(-1)
