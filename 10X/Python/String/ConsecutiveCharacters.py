ce03edab0fbf6e666b9f3075c018ae4ef9e129a3
Nishant Chandra <nishantchandra85@gmail.com>
2021-07-29 12:30:06 +0530

    .

--- 10X/Python/String/ConsecutiveCharacters.py
+++ 10X/Python/String/ConsecutiveCharacters.py
@@ -20,6 +20,18 @@ if l>0:
 else:
     print(0)
 
+"""
+        count=1
+        maxi=0
+        for i in range(1,len(s)):
+            if (s[i]==s[i-1]):
+                count+=1
+            else:
+                maxi=max(count,maxi)
+                count=1
+        print(max(count,maxi))
 
+
+"""
 
 
