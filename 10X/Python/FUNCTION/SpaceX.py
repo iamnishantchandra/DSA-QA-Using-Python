28fa90809915a2ceddc6b96e0e3073fdac8129fa
Nishant Chandra <nishantchandra85@gmail.com>
2021-08-02 19:40:37 +0530

    SpaceX

---
+++ 10X/Python/FUNCTION/SpaceX.py
@@ -0,0 +1,20 @@
+'''
+SpaceX
+You have been given a sentence, which will have space separated words. You task it to replace those space with spaceX, and print the modified string.
+
+Input
+One string, representing above sentence.
+
+Output
+One string, denoting result
+
+Example
+Input1:
+
+my name is john.
+Output1:
+
+myspaceXnamespaceXisspaceXjohn.
+'''
+s=0
+print(input().replace(" ","spaceX"))
