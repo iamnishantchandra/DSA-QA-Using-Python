ea139c46763604a9d9070b4f50e0b5d157b8f871
Nishant Chandra <nishantchandra85@gmail.com>
2021-09-06 09:42:36 +0530

    .

--- 10X/Python/String/SingleNumber.py
+++ 10X/Python/String/SingleNumber.py
@@ -1,4 +1,40 @@
+'''
+Single Number
+Given a list of N integers, every element appears twice except for one. Find that single one.
+
+Input
+First number is N (the number of integers given) Followed by the N numbers
+
+Output
+One line containing the output integer
+
+Example
+Input: 3
+
+2
+
+2
+
+1
+
+Output: 1
+
+Input: 5
+
+2
+
+2
+
+1
+
+1
+
+3
+
+Output: 3
+'''
 # Single Number
+'''
 n=int(input())
 a=[]
 Count=2
@@ -15,3 +51,23 @@ for k in a:
                 break
 
 print(p)   
+only-50%
+'''
+n=int(input())
+a=[]
+Count=2
+
+for j in range(n):
+   a.append(int(input()))
+a=sorted(a)
+p=a[0]
+f=False
+for i in range(n-1):
+    if a[i]!=a[i-1] and a[i]!=a[i+1]:
+        p=a[i]
+        f=True
+        break
+if f:
+    print(p)
+else:
+    print(a[n-1])
